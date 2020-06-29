bc24357fea5d363e9bf164dc58010f50c94fc83c
Dawid Labudda <50706896+dalabudda@users.noreply.github.com>
2020-06-29 11:43:42 +0200

    setting up 2

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const router = require('./routes')
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use('/', express.static(__dirname + '/public_html'))
+  .use('/api', router)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
