f9270db9ace38169ee3aa4d25ef418fe7da05237
Dawid Labudda <50706896+dalabudda@users.noreply.github.com>
2020-07-02 17:38:27 +0200

    .

--- routes.js
+++ routes.js
@@ -3,9 +3,9 @@ const users = require('./controllers/users.js');
 const reports = require('./controllers/reports.js');
 const router = express.Router();
 
-router.get('/users/init', user.init);
-router.get('/users/add', user.put);
-router.get('/users', user.get);
+router.get('/users/init', users.init);
+router.get('/users/add', users.put);
+router.get('/users', users.get);
 router.get('/reports', report.get);
 router.post('/reports', report.post);
 
