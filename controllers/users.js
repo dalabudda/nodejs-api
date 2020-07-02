c5e8a884b70beaf1c1cbea9a42bdf2696fea9b36
Dawid Labudda <50706896+dalabudda@users.noreply.github.com>
2020-07-02 18:01:39 +0200

    .

--- controllers/users.js
+++ controllers/users.js
@@ -6,10 +6,39 @@ let url = process.env.MONGODB_URI;
 users.init = (req, res) => {
     MongoClient.connect(url, (err, db) => {
         if (err) throw err;
-        db.createCollection("users", (err, res) => {
-            if (err) throw err;
-            console.log("Collection created!");
-            db.close();
+        db.collection('users', function (err, collection) {
+        
+            collection.insert({
+                id: 1,
+                employeeNumber: 123317,
+                firstName: "Tomasz",
+                secondName: "Krzysztof",
+                lastName: "Kowalski",
+                gender: "mężczyzna",
+                dateOfBirth: "1980-02-23T00:00:00.000Z",
+                street: "Pomorska",
+                propertyNumber: "12",
+                apartmentNumber: "7",
+                city: "Gdańsk",
+                zipCode: "80-000",
+                country: "Polska",
+                email: "tomasz.kowalski@dealer.toyota.pl",
+                privetPhone: 600300233,
+                businessPhone: 789000100,
+                jobPosition: "doradca sprzedaży flotowej",
+                workplace: "oddział Gdańsk, pomorska",
+                supervisor: true,
+                education: "średnie",
+                dateOfEmployment: "2010-01-01T00:00:00.000Z",
+                dateOfTermination: "",
+                employmentStatus: true
+              });
+    
+            db.collection('users').count(function (err, count) {
+                if (err) throw err;
+                
+                console.log('Total Rows: ' + count);
+            });
         });
     });
 };
@@ -17,12 +46,7 @@ users.init = (req, res) => {
 users.put = (req, res) => {
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
-        var myobj = { name: "Company Inc", address: "Highway 37" };
-        db.collection("customers").insertOne(myobj, function(err, res) {
-          if (err) throw err;
-          console.log("1 document inserted");
-          db.close();
-        });
+        
       });
 };
 
