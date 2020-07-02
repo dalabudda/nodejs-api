a68160dd69dfe8b9c03b661c8dc9fd964375eb38
Dawid Labudda <50706896+dalabudda@users.noreply.github.com>
2020-07-02 22:44:41 +0200

    .

--- controllers/users.js
+++ controllers/users.js
@@ -2,68 +2,61 @@ let users = {};
 
 let MongoClient = require('mongodb').MongoClient;
 let url = process.env.MONGODB_URI;
+let dbname = "heroku_hlcqgxf6";
+let dbo;
+MongoClient.connect(url, (err, db) => {
+    if (err) throw err;
+    dbo = db.db(dbname);
+});
 
 users.init = (req, res) => {
-    MongoClient.connect(url, (err, db) => {
+    dbo.createCollection("users", (err, result) => {
         if (err) throw err;
-        let dbo = db.db("heroku_hlcqgxf6");
-        dbo.createCollection("users", (err, result) => {
-            if (err) throw err;
-            console.log("Collection created!");
-            db.close();
-        });
+        console.log("Collection created!");
+        db.close();
     });
     res.send("Done");
 };
 
 users.put = (req, res) => {
-    MongoClient.connect(url, function(err, db) {
+    let myobj = {
+        id: 1,
+        employeeNumber: 123317,
+        firstName: "Tomasz",
+        secondName: "Krzysztof",
+        lastName: "Kowalski",
+        gender: "mężczyzna",
+        dateOfBirth: "1980-02-23T00:00:00.000Z",
+        street: "Pomorska",
+        propertyNumber: "12",
+        apartmentNumber: "7",
+        city: "Gdańsk",
+        zipCode: "80-000",
+        country: "Polska",
+        email: "tomasz.kowalski@dealer.toyota.pl",
+        privetPhone: 600300233,
+        businessPhone: 789000100,
+        jobPosition: "doradca sprzedaży flotowej",
+        workplace: "oddział Gdańsk, pomorska",
+        supervisor: true,
+        education: "średnie",
+        dateOfEmployment: "2010-01-01T00:00:00.000Z",
+        dateOfTermination: "",
+        employmentStatus: true
+    };
+    dbo.collection("users").insertOne(myobj, function(err, result) {
         if (err) throw err;
-        let dbo = db.db("heroku_hlcqgxf6");
-        let myobj = {
-            id: 1,
-            employeeNumber: 123317,
-            firstName: "Tomasz",
-            secondName: "Krzysztof",
-            lastName: "Kowalski",
-            gender: "mężczyzna",
-            dateOfBirth: "1980-02-23T00:00:00.000Z",
-            street: "Pomorska",
-            propertyNumber: "12",
-            apartmentNumber: "7",
-            city: "Gdańsk",
-            zipCode: "80-000",
-            country: "Polska",
-            email: "tomasz.kowalski@dealer.toyota.pl",
-            privetPhone: 600300233,
-            businessPhone: 789000100,
-            jobPosition: "doradca sprzedaży flotowej",
-            workplace: "oddział Gdańsk, pomorska",
-            supervisor: true,
-            education: "średnie",
-            dateOfEmployment: "2010-01-01T00:00:00.000Z",
-            dateOfTermination: "",
-            employmentStatus: true
-          };
-        dbo.collection("users").insertOne(myobj, function(err, result) {
-            if (err) throw err;
-            console.log("1 user inserted");
-            db.close();
-          });
-      });
-      res.send("Done");
+        console.log("1 user inserted");
+        db.close();
+    });
 };
 
 users.get = (req, res) => {
-    MongoClient.connect(url, function(err, db) {
+    dbo.collection("users").find({}).toArray(function(err, result) {
         if (err) throw err;
-        let dbo = db.db("heroku_hlcqgxf6");
-        dbo.collection("users").find({}).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            db.close();
-            res.send(JSON.stringify(result));
-          });
+        console.log(result);
+        db.close();
+        res.send(JSON.stringify(result));
     });
 };
 
