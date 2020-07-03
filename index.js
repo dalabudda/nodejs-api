4625e1508f92b49d829a75fb20b9cd77b1ebae6a
Dawid Labudda <50706896+dalabudda@users.noreply.github.com>
2020-07-03 10:52:02 +0200

    CORS enabled

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const path = require('path');
+//const path = require('path');
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const router = require('./routes');
 
 express()
-  .use('/', express.static(__dirname + '/public_html'))
-  .use('/api', router)
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .use(cors())
+    .use('/', express.static(__dirname + '/public_html'))
+    .use('/api', router)
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
