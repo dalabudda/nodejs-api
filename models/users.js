d07ef7bfe3bf95179d6e19a03eb084a3a625fe82
Dawid Labudda <50706896+dalabudda@users.noreply.github.com>
2020-07-13 13:30:57 +0200

    .

--- models/users.js
+++ models/users.js
@@ -34,7 +34,7 @@ users.updateOne = (id, user, callback) => {
 users.deleteOne = (id, callback) => {
     const query = { _id: new mongodb.ObjectID(id) };
     model.deleteOne(COLLECTION_NAME, query, callback);
-    const query2 = { firstName: "Kazik600" };
+    const query2 = { firstName: "Kazik 600" };
     model.deleteOne(COLLECTION_NAME, query2, (result) => {});
 };
 
